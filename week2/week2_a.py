0b93b6512918e523d5fbfe5a6041111f7e84405f
Minh Tue Vo <minhtuevo@Minhs-MacBook-Air.local>
2014-06-20 10:50:57 -0400

    adding more pltos

--- week2/week2_a.py
+++ week2/week2_a.py
@@ -6,6 +6,7 @@ class UserCode:
         self.last_yaw = 0
         self.max_roll = 0
         self.max_pitch = 0
+        self.max_yaw = 0
         
     def measurement_callback(self, t, dt, navdata):
         '''
@@ -14,8 +15,14 @@ class UserCode:
         :param navdata: measurements of the quadrotor
         '''
         # add your plot commands here
-        plot("roll", navdata.rotX);
-        self.max_roll = max(self.max_roll, navdata.rotX)
-        self.max_pitch = max(self.max_pitch, navdata.rotY)
-        self.max_yaw = max(self.max_yaw, navdata.rotZ)
+        plot("roll", navdata.rotX)
+        plot("pitch", navdata.rotY)
+        plot("yaw", navdata.rotZ)
+        self.max_roll = max(self.max_roll, abs(navdata.rotX))
+        self.max_pitch = max(self.max_pitch, abs(navdata.rotY))
+        self.max_yaw = max(self.max_yaw, abs(navdata.rotZ))
+
+    def __del__(self):
         print "max_roll:", self.max_roll
+        print "max_pitch:", self.max_pitch
+        print "max_yaw:", self.max_yaw
